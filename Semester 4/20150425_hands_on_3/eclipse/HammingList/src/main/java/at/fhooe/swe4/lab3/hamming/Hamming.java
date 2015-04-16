966df64d91ac37c8e56bb84d88adb20d935c1033
Thomas Herzog <herzog.thomas81@gmail.com>
2015-04-16 10:43:57 +0200

    - Finaly fixed hamming numbers which is now sophisticated and pretty fast - further dev on doc - moved junit-repot folder to doc (referenced by documentation)

--- Semester 4/20150425_hands_on_3/eclipse/HammingList/src/main/java/at/fhooe/swe4/lab3/hamming/Hamming.java
+++ Semester 4/20150425_hands_on_3/eclipse/HammingList/src/main/java/at/fhooe/swe4/lab3/hamming/Hamming.java
@@ -2,8 +2,9 @@ package at.fhooe.swe4.lab3.hamming;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.NavigableSet;
+import java.util.TreeSet;
 
 /**
  * Provides class methods which are used to handle hamming numbers.
@@ -13,6 +14,10 @@ import java.util.List;
  */
 public class Hamming {
 
+	private static final BigInteger second = BigInteger.valueOf(2);
+	private static final BigInteger three = BigInteger.valueOf(3);
+	private static final BigInteger five = BigInteger.valueOf(5);
+
 	/**
 	 * Not meant to be instantiated
 	 */
@@ -21,52 +26,35 @@ public class Hamming {
 	}
 
 	/**
-	 * Calculates the hamming numbers to the given count.
+	 * Calculates the 'count' hamming numbers.
 	 * 
 	 * @param count
-	 *            the count of hamming numbers to calculate
-	 * @return the sorted list containing the hamming numbers
-	 * @throws IllegalArgumentException
-	 *             if count <= 0
+	 *            the count of to calculate hamming numbers
+	 * @return the sorted list holding the hamming numbers
 	 */
 	public static List<BigInteger> calulcateHammingNumbers(final int count) {
 		// At least one is in the hamming list
 		if (count <= 1) {
 			throw new IllegalArgumentException("The count must be at least one !!!");
 		}
-		final List<BigInteger> list = new ArrayList<BigInteger>((count + 4));
-		list.add(BigInteger.ONE);
-		// The allowed factors
-		final BigInteger second = BigInteger.valueOf(2);
-		final BigInteger three = BigInteger.valueOf(3);
-		final BigInteger five = BigInteger.valueOf(5);
-
-		// As long as all of the intended numbers have been calculated
-		// Calculate for 4 more elements because otherwise some numbers would be
-		// missing
-		for (int i = 0; (list.size() < (count + 4)); i++) {
-			BigInteger secondMult = list.get(i).multiply(second);
-			BigInteger threeMult = list.get(i).multiply(three);
-			BigInteger fiveMult = list.get(i).multiply(five);
-			// Avoid duplicates of 2 * x
-			if (!list.contains(secondMult)) {
-				list.add(secondMult);
-			}
-			// Avoid duplicates of 3 * x
-			if (!list.contains(threeMult)) {
-				list.add(threeMult);
-			}
-			// Avoid duplicates of 5 * x
-			if (!list.contains(fiveMult)) {
-				list.add(fiveMult);
-			}
+		// Avoid grow of ArrayList
+		final List<BigInteger> list = new ArrayList<BigInteger>(count);
+		// Keeps calculated elements sorted
+		final NavigableSet<BigInteger> sortedSet = new TreeSet<BigInteger>();
+		// ONE is initial value
+		sortedSet.add(BigInteger.ONE);
+		// As long as we need to calculate
+		while (list.size() != count) {
+			// Get the next hammming number and remove from set
+			final BigInteger currentValue = sortedSet.pollFirst();
+			// Add this element to result lsit
+			list.add(currentValue);
+			// calculate next hamming numbers
+			sortedSet.add(currentValue.multiply(second));
+			sortedSet.add(currentValue.multiply(three));
+			sortedSet.add(currentValue.multiply(five));
 		}
-		Collections.sort(list);
-		// Remove the elements which are to much
-		for (int i = (list.size() - 1); i >= count; i--) {
-			list.remove(list.get(i));
-		}
+		// Returned list is implicitly sorted
 		return list;
 	}
-
 }
--- Semester 4/20150425_hands_on_3/eclipse/HammingList/src/test/java/at/fhooe/swe4/lab3/test/hamming/HammingTest.java
+++ Semester 4/20150425_hands_on_3/eclipse/HammingList/src/test/java/at/fhooe/swe4/lab3/test/hamming/HammingTest.java
@@ -32,23 +32,19 @@ public class HammingTest {
 	public void test_caluclation() {
 		int count = 1;
 		final int factor = 10;
-		final int repeation = 4;
+		final int repeation = 6;
 		for (int i = 0; i < repeation; i++) {
 			count *= factor;
 			final long startMillis = System.currentTimeMillis();
 			final List<BigInteger> result = Hamming.calulcateHammingNumbers(count);
 			final long diffMillis = System.currentTimeMillis() - startMillis;
 			System.out.println("----------------------------------------------------------------");
-			System.out.println(new StringBuilder("Spend time in millis: '").append(diffMillis).append("' for '").append(count)
-					.append("' hamming numbers (including sorting)").toString());
-			System.out.println("----------------------------------------------------------------");
-			for (int j = 0; j < result.size(); j++) {
+			System.out.println(new StringBuilder("Spend time in millis: '").append(diffMillis).append("' for '").append(count).append("' hamming numbers")
+					.toString());
+			for (int j = 0; (j < result.size()) && (j < 20); j++) {
 				System.out.println(new StringBuilder().append(j + 1).append(": ").append(result.get(j)).toString());
 			}
-			if (result.size() > 10) {
-				System.out.println("...");
-				System.out.println(new StringBuilder().append(result.size()).append(": ").append(result.get(result.size() - 1)).toString());
-			}
+			System.out.println("----------------------------------------------------------------");
 			System.out.println("----------------------------------------------------------------");
 		}
 	}
